edfe726d57dfe6537c8587be499ed2f98fd25316
Alessandro <descovi@gmail.com>
2020-09-17 12:37:12 +0200

    :construction: avoid double require

--- lib/amico-db.rb
+++ lib/amico-db.rb
@@ -2,7 +2,6 @@ require 'amico-db/configuration'
 require 'amico-db/version'
 require 'amico-db/download_tools/create_dir_if_not_exist'
 require 'amico-db/download'
-require 'amico-db/download_tools/create_dir_if_not_exist'
 require 'amico-db/dump'
 require 'amico-db/dump_cmd'
 require 'amico-db/railtie' if defined?(Rails::Railtie)
